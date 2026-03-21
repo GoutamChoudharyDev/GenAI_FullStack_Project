f98e7338dbb002b7c1f37ca0b403e5f606c596da
Goutam Choudhary <rc.goutam.choudhary@gmail.com>
2026-03-21 15:37:26 +0530

    changes

--- server/src/app.js
+++ server/src/app.js
@@ -10,32 +10,13 @@ const app = express();
 
 // Middlewares...........................
 // CORS Enable
-// app.use(cors({
-//     // origin: "http://localhost:5173",
-//     origin: "https://gen-ai-full-stack-project-git-main-goutam-choudhary-s-projects.vercel.app/",
-//     // origin: "https://gen-ai-full-stack-project-brown.vercel.app/",
-//     credentials: true
-// }));
-
-
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://gen-ai-full-stack-project-brown.vercel.app",
-    "https://gen-ai-full-stack-project-298a1y5ab-goutam-choudhary-s-projects.vercel.app"
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("CORS not allowed"));
-        }
-    },
+    origin: [
+        "http://localhost:5173",
+        "https://gen-ai-full-stack-project-298a1y5ab-goutam-choudhary-s-projects.vercel.app"
+    ],
     credentials: true
 }));
-
-app.options("*", cors());
 
 // Parse cookies and JSON bodies
 app.use(cookieParser());
